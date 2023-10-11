ca8af00283a39bf1f54c06b20be9e8a7749cf9c4
claraagui <aclarapao@gmail.com>
2023-10-10 20:02:22 -0600

    borre el segundo if

--- multiplos.py
+++ multiplos.py
@@ -8,7 +8,5 @@ if n1 % n2 == 0:
     print(f"El número {n1} es múltiplo del {n2}")
 elif n2 % n1 == 0:
     print(f"El número {n2} es múltiplo del {n1}")
-if n2 % n == 1:
-    print(f"El número {n2} es múltiplo del {n1}")
 else:
     print(f"Ninguno de los números es múltiplo del otro")
