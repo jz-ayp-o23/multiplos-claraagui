e5d655144889a37d223307c0db549566a0879c57
claraagui <aclarapao@gmail.com>
2023-10-14 13:14:44 -0600

    introducí otro if

--- multiplos.py
+++ multiplos.py
@@ -4,9 +4,12 @@
 n1 = int(input("Introduzca un número: "))
 n2 = int(input("Introduzca otro número: "))
 # Proceso
-if n1 % n2 == 0:
-    print(f"El número {n1} es múltiplo del {n2}")
-elif n2 % n1 == 0:
-    print(f"El número {n2} es múltiplo del {n1}")
+if n1 == 0 or n2 == 0:
+    print(f"Alguno de los dos números es cero")
 else:
-    print(f"Ninguno de los números es múltiplo del otro")
+    if n1 % n2 == 0:
+        print(f"El número {n1} es múltiplo del {n2}")
+    elif n2 % n1 == 0:
+        print(f"El número {n2} es múltiplo del {n1}")
+    else:
+        print(f"Ninguno de los números es múltiplo del otro")
